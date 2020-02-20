ef22d5d0e73bbc3d691cb233b482daec3ed5c94d
Anderson Raphael <phaell.anderson09@gmail.com>
2020-02-20 15:48:04 -0300

    alteração nos styled components de usuários

--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -42,7 +42,7 @@ export const Stars = styled.FlatList.attrs({
 `;
 
 export const Starred = styled.View`
-  background-color: #dddddd;
+  background-color: #eeeeee;
   border-radius: 4px;
   padding: 10px 15px;
   margin-bottom: 20px;
